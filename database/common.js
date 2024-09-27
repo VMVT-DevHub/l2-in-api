fc3907f3635c88e7e486bfb58bddfb90dd9e7172
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-09-27 10:25:56 +0300

    missing fields

--- database/common.js
+++ database/common.js
@@ -4,6 +4,9 @@ const commonFields = (table) => {
   table.timestamp('createdAt');
   table.timestamp('updatedAt');
   table.timestamp('deletedAt');
+  table.timestamp('createdBy');
+  table.timestamp('updatedBy');
+  table.timestamp('deletedBy');
 };
 
 const schema = process.env.DB_SCHEMA || 'public';
